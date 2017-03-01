c63e8e6ba9cf4264475d7a78b522d19c65b6077a
Ken Wheeler <ken_wheeler@me.com>
2017-03-01 17:08:38 -0500

    fixing prod build

--- configuration/babel/babel.prod.js
+++ configuration/babel/babel.prod.js
@@ -4,7 +4,7 @@ module.exports = {
     [ 'es2015', { loose: true, modules: false } ],
     'stage-0',
     'react'
-  ].map(require.resolve),
+  ],
   plugins: [ 'babel-plugin-transform-react-constant-elements' ]
     .map(require.resolve)
     .concat([ [ require.resolve('babel-plugin-transform-runtime') ] ])
